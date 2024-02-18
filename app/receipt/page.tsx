524609e88e372065cd721e4d1a801ab667c69afc
ymahendraa <ymahendra721@gmail.com>
2024-02-19 00:45:20 +0800

    feat (component): app/receipt/page.tsx => add remove receipt functionality

--- app/receipt/page.tsx
+++ app/receipt/page.tsx
@@ -4,11 +4,15 @@ import React from 'react'
 // hooks import
 import { useAppDispatch, useAppSelector } from '@/lib/hooks'
 
+// utils import
+import { addReceipt, removeReceipt } from '@/lib/features/receipt/receiptSlice'
+import { RootState } from '@/lib/store'
+import Link from 'next/link'
+
 // components import
 import Section from '@/components/atoms/section'
-import Link from 'next/link'
-import { RootState } from '@/lib/store'
-import { addReceipt } from '@/lib/features/receipt/receiptSlice'
+import Button from '@/components/atoms/button'
+import { TrashIcon } from '@heroicons/react/24/outline'
 
 /**
  * @description ReceiptPage component is a page to show all the receipts
@@ -32,11 +36,20 @@ const ReceiptPage = () => {
                     className='bg-primary text-white px-4 py-2 rounded-md'
                 >Tambah resep</Link>
             </Section>
-            <ul>
+            <ul className='flex gap-4 flex-col'>
                 {receipt.map((r) => (
-                    <li key={r.id} className='flex items-center gap-2'>
-                        <div className='bg-primary w-2 h-2 rounded-full' />
-                        <Link href={`/receipt/${r.id}`} key={r.id} className='hover:underline'>{r.name}</Link>
+                    <li key={r.id} className='flex items-center justify-between'>
+                        <Section className='flex gap-2 items-center'>
+                            <div className='bg-primary w-2 h-2 rounded-full' />
+                            <Link href={`/receipt/${r.id}`} key={r.id} className='hover:underline'>{r.name}</Link>
+                        </Section>
+                        <Button
+                            onClick={() => {
+                                dispatch(removeReceipt(r.id))
+                            }}
+                            className='bg-red-500 text-white px-2 py-1 rounded-md'
+                            icon={<TrashIcon className="w-4 h-4" />}
+                        />
                     </li>
                 ))}
             </ul>
